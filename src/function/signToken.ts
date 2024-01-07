2e1bc644829a76e183fa50c00d0071002489c035
Sami Fouad <sfouad@gmail.com>
2024-01-07 02:01:34 -0700

    adding jose dep for signing tokens

---
+++ src/function/signToken.ts
@@ -0,0 +1,23 @@
+import { userJwt } from "../interface/userJwt"
+import * as jose from 'jose'
+
+export async function signToken(user: userJwt, hmac: string, issuer: string, audience: string): Promise<string> {
+	const payload = {
+		role: user.role,
+		user: user.username,
+		email: user.email,
+	};
+
+	const secret = new TextEncoder().encode(hmac)
+	const alg = 'HS256'
+
+	const token = await new jose.SignJWT(payload)
+										.setProtectedHeader({ alg })
+										.setIssuedAt()
+										.setIssuer(issuer)
+										.setAudience(audience)
+										.setExpirationTime('24h')
+										.sign(secret)
+
+	return token;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 // constants
-export { regionMap } from "./const/regionMap";
-export { corsHeaders } from "./const/corsHeaders";
+export { regionMap } from "./const/regionMap"
+export { corsHeaders } from "./const/corsHeaders"
 
 // functions
-export { hashPassword } from "./function/hashPassword";
-export { readRequestBody } from "./function/readRequestBody";
+export { hashPassword } from "./function/hashPassword"
+export { readRequestBody } from "./function/readRequestBody"
+export { signToken } from "./function/signToken"
 
 // interfaces
-export { cookieOptions } from "./interface/cookieOptions";
-export { dbUser } from "./interface/dbUser";
-export { Env } from "./interface/Env";
-export { userJwt } from "./interface/userJwt";
-export { loginForm } from "./interface/loginForm";
+export { cookieOptions } from "./interface/cookieOptions"
+export { dbUser } from "./interface/dbUser"
+export { Env } from "./interface/Env"
+export { userJwt } from "./interface/userJwt"
+export { loginForm } from "./interface/loginForm"
